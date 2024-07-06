bb95470fa385189cdacae012330b80d8e6d05d36
codesche <codesche@gmail.com>
2024-07-07 00:43:33 +0900

    feat: 엔티티 구성 완료

--- src/main/java/com/example/msblog/domain/Article.java
+++ src/main/java/com/example/msblog/domain/Article.java
@@ -10,6 +10,7 @@ public class Article {
 
     @Id                                                                     // Id 필드를 기본키로 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY)                     // 기본키를 자동으로 1씩 증가시키기
+    @Column(name = "id", updatable = false)
     private Long id;
 
     @Column(name = "title", nullable = false)                               // 'title' 이라는 not null 컬럼과 매핑
@@ -19,7 +20,7 @@ public class Article {
     private String content;
 
     @Builder    // 빌더 패턴으로 작성
-    public Article(Long id, String title, String content) {
+    public Article(String title, String content) {
         this.title = title;
         this.content = content;
     }
