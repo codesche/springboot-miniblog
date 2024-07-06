c22c133ad7db57bcd8db7463cf7e43de5c9e468c
codesche <codesche@gmail.com>
2024-07-07 00:31:54 +0900

    Project Setting

---
+++ src/test/java/com/example/msblog/MsblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
