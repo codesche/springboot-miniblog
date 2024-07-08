3c6d4461715b0768e5eda42046c30be133f5058e
codesche <codesche@gmail.com>
2024-07-09 00:01:30 +0900

    feat: 모든 글 조회하는 메서드 코드 작성

--- src/main/java/com/example/msblog/service/BlogService.java
+++ src/main/java/com/example/msblog/service/BlogService.java
@@ -6,6 +6,8 @@ import com.example.msblog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor                // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service                                // 빈으로 등록
 public class BlogService {
@@ -15,6 +17,11 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 모든 글 조회
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 
 }
