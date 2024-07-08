d1b3be36a987995ef628a6d69253a7cc4223f8f8
codesche <codesche@gmail.com>
2024-07-09 00:04:31 +0900

    feat: 응답을 위한 DTO 작성

--- src/main/java/com/example/msblog/dto/ArticleResponse.java
+++ src/main/java/com/example/msblog/dto/ArticleResponse.java
@@ -4,13 +4,13 @@ import com.example.msblog.domain.Article;
 import lombok.Getter;
 
 @Getter
-public class ArtiCleResponse {
+public class ArticleResponse {
 
     private final String title;
     private final String content;
 
     // 엔티티를 인수로 받는 생성자 추가
-    public ArtiCleResponse(Article article) {
+    public ArticleResponse(Article article) {
         this.title = article.getTitle();
         this.content = article.getContent();
     }
