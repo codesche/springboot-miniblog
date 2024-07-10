640601eef9c9abefe62322ae31fa50a3b4f4d099
codesche <codesche@gmail.com>
2024-07-11 01:04:49 +0900

    feat: 업데이트 기능 테스트 코드 작성

--- src/test/java/com/example/msblog/controller/BlogApiControllerTest.java
+++ src/test/java/com/example/msblog/controller/BlogApiControllerTest.java
@@ -2,7 +2,9 @@ package com.example.msblog.controller;
 
 import com.example.msblog.domain.Article;
 import com.example.msblog.dto.AddArticleRequest;
+import com.example.msblog.dto.UpdateArticleRequest;
 import com.example.msblog.repository.BlogRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -147,5 +149,37 @@ class BlogApiControllerTest {
     assertThat(articles).isEmpty();
     }
 
+
+    @DisplayName("updateArticle: 블로그 글 수정에 성공한다.")
+    @Test
+    public void updateArticle() throws Exception {
+    // given
+    final String url = "/api/articles/{id}";
+    final String title = "title";
+    final String content = "content";
+
+    Article savedArticle = blogRepository.save(Article.builder()
+                    .title(title)
+                    .content(content)
+                    .build());
+
+    final String newTitle = "new title";
+    final String newContent = "new content";
+
+    UpdateArticleRequest request = new UpdateArticleRequest(newTitle, newContent);
+
+    // when
+    ResultActions result = mockMvc.perform(put(url, savedArticle.getId())
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(objectMapper.writeValueAsString(request)));
+
+    // then
+    result.andExpect(status().isOk());
+
+    Article article = blogRepository.findById(savedArticle.getId()).get();
+
+    assertThat(article.getTitle()).isEqualTo(newTitle);
+    assertThat(article.getContent()).isEqualTo(newContent);
+    }
 }
 
