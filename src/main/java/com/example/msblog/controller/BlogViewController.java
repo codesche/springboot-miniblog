6d60d92c07d72d87ed8bc11724c3dc287557a6b8
codesche <codesche@gmail.com>
2024-07-17 01:13:18 +0900

    feat: 수정 화면 보여주기 위한 컨트롤러 작성

--- src/main/java/com/example/msblog/controller/BlogViewController.java
+++ src/main/java/com/example/msblog/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -37,6 +38,19 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    // id 키를 가진 쿼리 파라미터 값을 id 변수에 매핑(id가 없을 수도 있다)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {       // id가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {                // id가 없으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 
 }
