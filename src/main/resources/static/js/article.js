60068e5874ce808671b22bc8d6dc0659c3ecdce5
codesche <codesche@gmail.com>
2024-07-17 01:28:02 +0900

    feat: 수정, 생성 기능 위한 API 구현

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -14,3 +14,30 @@ if (deleteButton) {
         })
     })
 }
+
+// 수정 기능
+// id가 modify-btn인 엘리먼트 조회
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    // 클릭 이벤트가 감지되면 수정 API 요청
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {      // fetch 메서드를 통해 수정 API로 /api/articles/ PUT 요청 보냄
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",     // 요청 형식 지정
+            },
+            body: JSON.stringify({              // title과 content를 가져옴
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+        .then(() => {
+            alert('수정이 완료되었습니다.');
+            location.replace(`articles/${id}`);
+        });
+    });
+}
