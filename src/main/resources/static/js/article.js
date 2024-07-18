1eec1ec9abf208aa0849a89b431c48865d1aac4b
codesche <codesche@gmail.com>
2024-07-19 00:04:36 +0900

    feat: 글 생성 기능 추가

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -41,3 +41,42 @@ if (modifyButton) {
         });
     });
 }
+
+// 생성 기능
+const createButton = document.getElementById("create-btn");
+
+if (createButton) {
+    // 클릭 이벤트 감지 시 생성 API 요청
+    createButton.addEventListener("click", (event) => {
+        fetch("/api/articles", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById("title").value,
+                content: document.getElementById("content").value,
+            }),
+        }).then(() => {
+            alert("등록 완료되었습니다.");
+            location.replace("/articles");
+        })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
