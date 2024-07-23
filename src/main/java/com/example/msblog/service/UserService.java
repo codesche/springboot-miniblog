18d071c3bb1cc3ea9a34a82159a5aaf3f2b9fe83
codesche <codesche@gmail.com>
2024-07-24 01:26:24 +0900

    feat: 회원 정보 추가 메서드 작성 - 서비스

---
+++ src/main/java/com/example/msblog/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.msblog.service;
+
+import com.example.msblog.domain.User;
+import com.example.msblog.dto.AddUserRequest;
+import com.example.msblog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
