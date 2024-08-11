1d6a683078168d101b1750aa88e898b0c871cbcf
codesche <codesche@gmail.com>
2024-08-11 16:00:21 +0900

    fix: 메소드명, 메서드 뷰 수정

--- src/main/java/com/example/msblog/controller/UserViewController.java
+++ src/main/java/com/example/msblog/controller/UserViewController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ViewController {
+public class UserViewController {
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return "oauthLogin";
     }
 
     @GetMapping("/signup")
